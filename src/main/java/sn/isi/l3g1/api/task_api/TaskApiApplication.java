37b63cbf97d9bbc811eb4c0207b785b4435f4afe
modev1006 <modev1006@gmail>
2026-02-27 02:43:14 +0000

    Add @ComponentScan to scan sn.isi.l3g1 packages

--- src/main/java/sn/isi/l3g1/api/task_api/TaskApiApplication.java
+++ src/main/java/sn/isi/l3g1/api/task_api/TaskApiApplication.java
@@ -2,12 +2,12 @@ package sn.isi.l3g1.api.task_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"sn.isi.l3g1"})
 public class TaskApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TaskApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApiApplication.class, args);
+    }
 }
